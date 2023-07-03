08bb0d65bf27dcbcf26a6501a802693408c0d386
Eric Doering <ericbenjamindoering@gmail.com>
2023-07-03 00:49:48 -0400

    form styles added, MessageForm created

--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -1,15 +1,17 @@
 "use client";
 
+import { MessageForm } from "../../components/messageForm";
 import React, { useState, ChangeEvent, FormEvent } from "react";
 
 export default function Register(): JSX.Element {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
+  const [userSubmit, setUserSubmit] = useState(false)
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-
+    setUserSubmit(true)
     setFirstName("");
     setLastName("");
     setPhoneNumber("");
@@ -28,38 +30,79 @@ export default function Register(): JSX.Element {
   };
 
   return (
-    <div>
-      <h1>Register</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          First Name:
-          <input
-            type="text"
-            value={firstName}
-            onChange={handleFirstNameChange}
-          />
+  <>
+  {userSubmit ? null : 
+    <div className="mt-40 w-small ml-80 mr-80 mx-auto">
+  <form className="border-indigo-600" onSubmit={handleSubmit}>
+    <div className="grid md:flex-col">
+      <div className="relative z-0 w-full mb-6 group">
+        <input
+          type="text"
+          name="floating_first_name"
+          id="floating_first_name"
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          placeholder=" "
+          value={firstName}
+          onChange={handleFirstNameChange}
+          required
+        />
+        <label
+          htmlFor="floating_first_name"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >
+          First name
         </label>
-        <br />
-        <label>
-          Last Name:
-          <input
-            type="text"
-            value={lastName}
-            onChange={handleLastNameChange}
-          />
+      </div>
+      <div className="relative z-0 w-full mb-6 group">
+        <input
+          type="text"
+          name="floating_last_name"
+          id="floating_last_name"
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          placeholder=" "
+          value={lastName}
+          onChange={handleLastNameChange}
+          required
+        />
+        <label
+          htmlFor="floating_last_name"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >
+          Last name
         </label>
-        <br />
-        <label>
-          Phone Number:
-          <input
-            type="tel"
-            value={phoneNumber}
-            onChange={handlePhoneNumberChange}
-          />
+      </div>
+      <div className="relative z-0 w-full mb-6 group">
+        <input
+          type="tel"
+          pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+          name="floating_phone"
+          id="floating_phone"
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          placeholder=" "
+          value={phoneNumber}
+          onChange={handlePhoneNumberChange}
+          required
+        />
+        <label
+          htmlFor="floating_phone"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >
+          Phone number (123-456-7890)
         </label>
-        <br />
-        <button type="submit">Submit</button>
-      </form>
+      </div>
     </div>
+    <button
+      type="submit"
+      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+    >
+      Submit
+    </button>
+  </form>
+</div>
+}
+<div>
+    {userSubmit ? <MessageForm /> : null}
+  </div>
+</>
   );
 }
---
+++ frontend/components/messageForm.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export function MessageForm(): JSX.Element{
+    return (
+        <>
+            <h1>Message Data</h1>
+        </>
+    )
+}
