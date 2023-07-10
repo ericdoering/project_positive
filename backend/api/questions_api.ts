a27e69a3a6dbe5ab9f6434b80e8293065a45e62f
Eric Doering <ericbenjamindoering@gmail.com>
2023-07-09 22:09:25 -0400

    added Questions API

---
+++ backend/api/questions_api.ts
@@ -0,0 +1,15 @@
+
+export const questions = [
+    ["What was the best part of your day today?"],
+    ["What is one thing that is going well in your life?"],
+    ["What was one healthy activity or action you did today?"],
+    ["What made you smile today?"],
+    ["What was one thing you learned today?"],
+    ["What was something new and novel that happend today?"],
+    ["What was the best thing you ate or drank today?"],
+    ["What beautiful thing did you see today?"],
+    ["What was a good song or podcast that you listened to recently?"],
+    ["Who did you help recently?"],
+    ["Who made you laugh or smile recently?"],
+    ["What are you grateful for?"]
+]
--- frontend/components/TimeForm.tsx
+++ frontend/components/TimeForm.tsx
@@ -1,9 +1,9 @@
-
-"use client"
+"use client";
 
 import React, { useState } from 'react';
 import dayjs, { Dayjs } from 'dayjs';
-import { TimePicker } from '@mui/x-date-pickers';
+import { LocalizationProvider, TimePicker } from '@mui/lab';
+import AdapterDayjs from '@mui/lab/AdapterDayjs';
 
 interface TimeFormProps {
   onSubmit: (time: string) => void;
@@ -19,18 +19,20 @@ export const TimeForm: React.FC<TimeFormProps> = ({ onSubmit }) => {
 
   return (
     <>
-        <div className="bg-gray-100 p-10 rounded form-size shadow-lg">
-        <TimePicker
-            label="Uncontrolled picker"
-            defaultValue={dayjs('2022-04-17T15:30')}
-        />
-        <TimePicker
-            label="Controlled picker"
-            value={value}
-            onChange={(newValue: dayjs.Dayjs) => setValue(newValue as Dayjs)}
-        />
-        <button onClick={handleSubmit}>Submit</button>
-        </div>
+      <div className="bg-gray-100 p-10 rounded form-size shadow-lg">
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <TimePicker
+              label="Uncontrolled picker"
+              defaultValue={dayjs('2022-04-17T15:30')}
+            />
+            <TimePicker
+              label="Controlled picker"
+              value={value}
+              onChange={(newValue: dayjs.Dayjs) => setValue(newValue as Dayjs)}
+            />
+            <button onClick={handleSubmit}>Submit</button>
+        </LocalizationProvider>
+      </div>
     </>
   );
 };
