0873df24964a2cd26a7f9510647287d7a959969a
Eric Doering <ericbenjamindoering@gmail.com>
2023-07-15 18:10:34 -0400

    setup of calls to action api

---
+++ backend/api/calls_to_action.ts
@@ -0,0 +1,17 @@
+export const callsToAction = [
+    ["Take 5 deep breaths."],
+    ["Stand up and stretch."],
+    ["Go drink some water."],
+    ["Call a friend or family member."],
+    ["Start a friendly conversation with someone around you."],
+    ["Go take a 5 minute walk."],
+    ["Listen to one of your favorite songs."],
+    ["Take a minute to admire something in the immediate facinity."],
+    ["Close your eyes and relax for 30 seconds."],
+    ["Think back to a very happy time."],
+    ["Do some light exercise."],
+    ["Text a friend or family member."],
+    ["Go fix yourself a healthy snack."],
+    ["Correct your current posture and situp straight."],
+    ["Do something that pushes you out of your comfort zone."]
+];
