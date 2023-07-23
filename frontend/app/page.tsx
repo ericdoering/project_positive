8a469a096d80a8927c9e1aa474799272a07d3f15
Eric Doering <ericbenjamindoering@gmail.com>
2023-07-23 10:10:06 -0400

    summary content created

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,14 @@
 
+import { Summary } from "../components/Summary"
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <h1>Some Content</h1>
-    </div>
+    <>
+      <div className="w-5/6 p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <h5 className="mb-2 text-3xl font-bold text-gray-900 dark:text-white">Project Positive</h5>
+        <Summary />
+      </div>
+    </>
   )
 };
+
---
+++ frontend/components/Summary.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+export function Summary(): JSX.Element {
+    return (
+        <>
+            <p className="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">
+            Setting up a personal gratitude practice is crucial for mental health, 
+            as it can shift focus from negativity to positivity, enhances positive emotions, 
+            promotes resilience, improves relationships, reduces stress, increases self-awareness, 
+            fosters empathy and compassion, enhances sleep quality, and cultivates a mindful mindset. 
+            Regularly expressing gratitude leads to increased happiness, contentment, and life satisfaction, 
+            while also reducing anxiety and depression. By acknowledging and appreciating 
+            the positive aspects of life, individuals build stronger social connections, 
+            develop a more optimistic outlook, and cope better with challenging situations, 
+            ultimately contributing to improved mental and emotional well-being.
+            </p>
+        </>
+    )
+
+}
