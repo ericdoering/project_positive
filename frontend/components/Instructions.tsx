264cf00e5d941fe79a2d871b3ae3b9150d2c866c
Eric Doering <ericbenjamindoering@gmail.com>
2023-07-25 20:31:09 -0400

    Instructions component added

---
+++ frontend/components/Instructions.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+export function Instructions(): JSX.Element {
+    return (
+        <>
+            <p className="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">
+                Project Positive helps users create a gratitude practice by setting up automatated
+                messages that inspire users to feel grateful and to practice healthy habits.
+                Users can pick when they want to recieve texts and what content they would like to recieve.
+                Content includes inspirational quotes, healthy calls to action, and thought provoking questions
+                (See Examples Below). Project Positive helps promote the habit of graditude. Just fill out 
+                the form and take the next step on improving your mental health.
+            </p>
+        </>
+    )
+
+}
