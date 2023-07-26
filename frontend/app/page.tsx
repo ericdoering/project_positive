0fb6da040c6286ab4d987cf7ff4942375b156d51
Eric Doering <ericbenjamindoering@gmail.com>
2023-07-26 13:33:25 -0400

    MessageExample component created

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,23 +1,24 @@
 
 "use client"
 
-import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-
 import { Instructions } from "../components/Instructions";
 import { Summary } from "../components/Summary";
+import { MessageExamples } from "../components/MessageExamples";
 
 export default function Home() {
   return (
     <>
       <div className="flex flex-col items-center justify-center">
-          <div className="w-5/6 p-4 text-center large-margin bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+          <div className="w-5/6 p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
             <h5 className="mb-2 text-3xl font-bold text-gray-900 dark:text-white">Project Positive</h5>
             <Summary />
           </div>
           <div className="w-5/6 p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
             <h5 className="mb-2 text-3xl font-bold text-gray-900 dark:text-white">How it Works</h5>
             <Instructions />
+          </div>
+          <div>
+            <MessageExamples />
           </div>
       </div>
     </>
--- frontend/components/ReviewForms.tsx
+++ frontend/components/ReviewForms.tsx
@@ -43,7 +43,7 @@ interface User {
   
     return (
       <>
-        <div className="flex items-center justify-center">
+        <div className="flex flex-col items-center justify-center">
             <div className="rounded bg-gray-100 shadow-lg">
             <div className="review-heading">Review Information</div>
                 <div className="text-center">
@@ -97,7 +97,7 @@ interface User {
                 </button>
                 </div>
                 <button
-                    className="text-white bg-indigo-700 hover:bg-indigo-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    className="text-white w-3/5 bg-indigo-700 hover:bg-indigo-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
                  Submit
                 </button>
