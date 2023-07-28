7c32cfda2472baf6d00ab0c367d88aa4ee30f9b8
Eric Doering <ericbenjamindoering@gmail.com>
2023-07-28 14:12:41 -0400

    partially completed scroll effect

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,29 +1,62 @@
+"use client";
 
-"use client"
-
+import { useEffect, useRef } from "react";
 import { Instructions } from "../components/Instructions";
 import { Summary } from "../components/Summary";
 import { MessageExamples } from "../components/MessageExamples";
 
-export default function Home() {
+const Home: React.FC = () => {
+  const cardRefs = useRef<HTMLElement[]>([]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        } else {
+          entry.target.classList.remove("show");
+        }
+      });
+    });
+
+    cardRefs.current.forEach((card) => {
+      if (card) {
+        observer.observe(card);
+      }
+    });
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
   return (
     <>
-      <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center card-container">
+        <section ref={(el) => (cardRefs.current[0] = el)} className="flex flex-col items-center container justify-center card">
           <div className="w-5/6 p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
             <h5 className="mb-2 text-3xl font-bold text-gray-900 dark:text-white">Project Positive</h5>
             <Summary />
           </div>
+        </section>
+        <section ref={(el) => (cardRefs.current[1] = el)} className="flex flex-col items-center container justify-center card">
           <div className="w-5/6 p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
             <h5 className="mb-2 text-3xl font-bold text-gray-900 dark:text-white">How it Works</h5>
             <Instructions />
           </div>
+        </section>
+        <section ref={(el) => (cardRefs.current[2] = el)} className="flex flex-col items-center container justify-center card">
           <div>
             <MessageExamples />
           </div>
+        </section>
       </div>
     </>
   );
 };
+
+export default Home;
+
 
 
 
