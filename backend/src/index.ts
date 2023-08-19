b6fd80b1f30f3dad63ea03afda91bfe4f922349c
Eric Doering <ericbenjamindoering@gmail.com>
2023-08-19 15:03:22 -0400

    user payload successfully populates user table

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import pool from "../database/db";
+import bodyParser from "body-parser";
 
 
 
@@ -21,9 +22,56 @@ app.get('/', (req, res)=> {
     res.send("Welcome to root URL of Server");
 });
 
+// Middleware to parse JSON body
+app.use(bodyParser.json());
 
-app.post("/register", (req, res) => {
-    console.log(req.body);
-    res.status(200);
-})
+
+app.post("/register", async (req, res) => {
+
+try {
+
+    const Userpayload  = [
+        req.body.firstName,
+        req.body.lastName,
+        req.body.phoneNumber,
+    ];
+
+    const messagePayload = [
+        req.body.days,
+        req.body.time,
+        req.body.Messages
+    ];
+
+
+
+    // if (!payload) {
+    //   return res.status(400).json({ error: 'Payload is required.' });
+    // }
+
+    // Acquire a client from the pool
+    const client = await pool.connect();
+
+    // Insert data into the database within a transaction
+    try {
+      await client.query('BEGIN');
+      const query = 'INSERT INTO users (first_name, last_name, phone_number, is_active) VALUES ($1, $2, $3, true);';
+      await client.query(query, Userpayload);
+      await client.query('COMMIT');
+    } catch (error) {
+      await client.query('ROLLBACK');
+      throw error;
+    } finally {
+      // Release the client back to the pool
+      client.release();
+    }
+
+    res.status(201).json({ message: 'Data added successfully.' });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'An error occurred while adding data.' });
+  }
+});
+
+
+
 
