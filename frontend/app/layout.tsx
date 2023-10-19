1a0b2feaccbcdf8839630e19939d79a8aef6fb55
Eric Doering <ericbenjamindoering@gmail.com>
2023-10-18 21:22:34 -0400

    responsive design 95% complete

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
             <nav style={{ height: "50px", position:'fixed'}} className="bg-gray-800 dark:bg-gray-900 font-bold fixed w-full top-0 left-0 border-b border-gray-200 dark:border-gray-600 flex items-center nav z-10">
               <div className="w-full sm:w-20 md:w-32 lg:w-48 flex items-center justify-evenly mx-auto p-4 nav-font">
               <HomeLink />
-              <Link className="sm:ml-2 md:ml-6 text-sm sm:text-base md:text-lg lg:text-xl" href="/register">
+              <Link className="sm:ml-2 md:ml-6 text-sm sm:text-base md:text-lg lg:text-xl nav-el rg-bt" href="/register">
                 Register
               </Link>
               </div>
@@ -50,7 +50,7 @@ export default function RootLayout({
     return (
       <>
         <Refresh />
-        <Link className="text-sm sm:text-base md:text-lg lg:text-xl" href="/" onClick={handleHomeClick}>
+        <Link className="text-sm sm:text-base md:text-lg lg:text-xl nav-el hm-bt" href="/" onClick={handleHomeClick}>
           Home
         </Link>
       </>
--- frontend/components/MessageExamples.tsx
+++ frontend/components/MessageExamples.tsx
@@ -13,21 +13,21 @@ export function MessageExamples(): JSX.Element {
             <h2 className="msg-examples">Message Examples</h2>
         </div>
             <div className="marg-bg msg-ex-crd ">
-                <div className="flex flex-row items-center bg-white border border-gray-900 rounded-lg shadow md:flex-row md:max-w-xl mb-6 anti-warp">
+                <div className="flex flex-row items-center bg-white border border-gray-900 rounded-lg shadow md:flex-row md:max-w-xl mb-6 anti-warp ex-crds">
                 <Image className="msg-exp" height={100} width={100} src={oprah} alt="oprah" />
                     <div className="flex flex-col justify-between p-4 leading-normal">
                         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Motivational Quotes</h5>
                         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">“The more you praise and celebrate your life, the more there is in life to celebrate. ” — Oprah Winfrey</p>
                     </div>
                 </div>
-                <div className="flex flex-row items-center bg-white border border-gray-900 rounded-lg shadow md:flex-row md:max-w-xl mb-6">
+                <div className="flex flex-row items-center bg-white border border-gray-900 rounded-lg shadow md:flex-row md:max-w-xl mb-6 ex-crds">
                 <Image className="msg-exp" height={100} width={100} src={breath} alt="deep breathing" />
                     <div className="flex flex-col justify-between p-4 leading-normal">
                         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Calls to Action</h5>
                         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Stop and take 5 deep breaths.</p>
                     </div>
                 </div>
-                <div className="flex flex-row items-center bg-white border border-gray-900 rounded-lg shadow md:flex-row md:max-w-xl mb-6">
+                <div className="flex flex-row items-center bg-white border border-gray-900 rounded-lg shadow md:flex-row md:max-w-xl mb-6 ex-crds">
                 <Image className="msg-exp" height={100} width={100} src={helpful} alt="helping person" />
                     <div className="flex flex-col justify-between p-4 leading-normal">
                         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Graditude Questions</h5>
